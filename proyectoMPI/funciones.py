6ff49e1dfd85532dcac154436ee8b50f982c6de3
EmanuelGP <gpe_fc@uaem.mx>
2015-02-16 21:31:46 -0600

    Actualizacion de las funciones que calculan los parametros variables de la red.

--- proyectoMPI/funciones.py
+++ proyectoMPI/funciones.py
@@ -1,26 +1,50 @@
-def suma_ponderada():
-	pass
+import math
 
-def identidad_lineal():
-	pass
+def suma_ponderada(bias, entrada, pesos):
+	"""
+	"""
+	potencial = 0.0
+	for i in range(len(pesos)):
+		potencial += (pesos[i] * entrada[i])
+	potencial += bias
+	return potencial
 
-def sigmoide_logistico():
-	pass
+def identidad_lineal(potencial):
+	"""
+	"""
+	return potencial
 
-def sigmoide_tangencial():
-	pass
+def sigmoide_logistico(potencial):
+	"""
+	"""
+	return (1.0 / (1.0 + (pow(e, -1 * potencial))))
 
-def sigmoide_hiperbolico():
-	pass
+def sigmoide_tangencial(potencial):
+	"""
+	"""
+	return ((2.0 / (1.0 + pow(e, -1 * pow))) - 1.0)
 
-def derivada_lineal():
-	pass
+def sigmoide_hiperbolico(potencial):
+	"""
+	"""
+	return ((pow(e, potencial) - (pow(e, -1 * potencial))) / (pow(e, potencial) + (pow(e, -1 * potencial))))
 
-def derivada_logistica():
-	pass
+def derivada_lineal(potencial):
+	"""
+	"""
+	return 1.0
 
-def derivada_tangencial():
-	pass
+def derivada_logistica(potencial):
+	"""
+	"""
+	return (sigmoide_logistico(potencial) * (1.0 - sigmoide_logistico(potencial)))
 
-def derivada_hiperbolica():
-	pass
+def derivada_tangencial(potencial):
+	"""
+	"""
+	return ((2.0 * pow(e, -1 * potencial)) / pow(1.0 + pow(e, -1 * pow), 2))
+
+def derivada_hiperbolica(potencial):
+	"""
+	"""
+	return (1.0 - pow(sigmoide_hiperbolico(potencial), 2))
---
+++ proyectoMPI/red.py
@@ -0,0 +1,46 @@
+class RedNeuronal:
+	"""
+	"""
+
+	def __init__(self):
+		"""
+		"""
+		self.__nombre_red = ""
+		self.__configuracion_alphas = ""
+		self.__configuracion_funciones = ""
+		self.__total_neuronas_capa = []
+		self.__indice_funcion_activacion = []
+		self.__red_neuronal = []
+
+	def establecer_nombre_red(self):
+		pass
+
+	def establecer_configuracion_alphas(self):
+		pass
+
+	def establecer_configuracion_funciones(self):
+		pass
+
+	def establecer_indice_funcion_activacion(self):
+		pass
+
+	def establecer_red_neuronal(self):
+		pass
+
+	def obtener_nombre_red(self):
+		pass
+
+	def obtener_red_neuronal(self):
+		pass
+
+	def aplicar_red_neuronal(self):
+		pass
+
+	def realizar_propagacion(self):
+		pass
+
+	def realizar_retropropagacion(self):
+		pass
+
+	def actualizar_parametros_neuronales(self):
+		pass
