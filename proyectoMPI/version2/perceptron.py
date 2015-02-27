5ff541c4d744b4c3a88efabe0f0003442ec68458
EmanuelGP <emanuel.garp@gmail.com>
2015-02-27 06:05:55 -0600

    Actualizacion.

--- proyectoMPI/version2/perceptron.py
+++ proyectoMPI/version2/perceptron.py
@@ -1,41 +1,15 @@
-"""
-Modulo que contiene las clases necesarias para crear objetos de redes
-neuronales artificiales de tipo perceptron multicapa y ser entrenadas
-con un algoritmo de retropropagacion.
-"""
-
-
 from funciones import *
 
-
 class Neurona:
-	"""
-	Neurona artificial tipo perceptron.
-	"""
-
 	def __init__(self, total_args):
-		"""
-		total_args        Numero total de valores de los pesos sinapticos
-		                  de la neurona.
-
-		Inicializa los datos miembro de la neurona.
-		"""
 		self.alpha = 0.0
 		self.salida = 0.0
-		self.bias = pseudoaleatorio(-1.0, 1.0)
+		self.bias = pseudoaleatorio(-0.9, 0.9)
 		self.pesos = []
 		for i in range(total_args):
-			self.pesos.append(pseudoaleatorio(-1.0, 1.0))
+			self.pesos.append(pseudoaleatorio(-0.9, 0.9))
 
 	def calcular_salida(self, id_funcion, entrada):
-		"""
-		id_funcion        Indice de la funcion de activacion asociada a la 
-		                  neurona.
-
-		entrada           Entrada presinaptica de la neurona.
-
-		Calcula la salida postsinaptica de la neurona.
-		"""
 		if id_funcion == 1:
 			self.salida = identidad_lineal(suma_ponderada(self.bias, entrada, self.pesos))
 		elif id_funcion == 2:
@@ -47,61 +21,22 @@ class Neurona:
 
 
 class CapaNeuronal:
-	"""
-	Capa de neuronas artificiales tipo perceptron.
-	"""
-
 	def __init__(self, total_neurs, total_args):
-		"""
-		total_neurs        Numero total de neuronas de la capa.
-
-		total_args        Numero total de valores de los pesos sinapticos
-		                  de cada neurona de la capa.
-
-		Inicializa los datos miembro de la capa.
-		"""
 		self.delthas = [0.0] * total_neurs
 		self.neuronas = []
 		for i in range(total_neurs):
 			self.neuronas.append(Neurona(total_args))
 
-	def establecer_alphas(self, alphas):
-		"""
-		"""
-		for i in range(len(alphas)):
-			self.neuronas[i].establecer_alpha(alphas[i])
-
 	def actualizar_biases(self):
-		"""
-		Actualiza el bias de cada neurona de la capa.
-		"""
 		for i in range(len(self.neuronas)):
 			self.neuronas[i].bias += (self.neuronas[i].alpha * self.delthas[i])
 
 	def actualizar_pesos(self, entrada):
-		"""
-		entrada        Entrada presinaptica de cada neurona de la capa.
-
-		Actualiza los pesos sinapticos de cada neurona de la capa.
-		"""
 		for i in range(len(self.neuronas)):
 			for j in range(len(entrada)):
 				self.neuronas[i].pesos[j] -= (self.neuronas[i].alpha * self.delthas[i] * entrada[j])
 
 	def calcular_delthas_salida(self, id_funciones, errores, entrada):
-		"""
-		id_funciones        Vector de indices que indican la funcion de 
-		                    activacion asociada a la neurona.
-
-		errores             Vector de errores obtenidos en las neuronas
-		                    de la capa de salida de la red.
-
-		entrada             Vector de valores presinapticos de entrada de
-		                    la capa.
-
-		Calcula los errores deltha cometidos por cada neurona de la capa
-		de salida de la red.
-		"""
 		for i in range(len(errores)):
 			if id_funciones[i] == 1:
 				self.delthas[i] = errores[i] * derivada_lineal(suma_ponderada(self.neuronas[i].bias, entrada, self.neuronas[i].pesos))
@@ -113,23 +48,10 @@ class CapaNeuronal:
 				self.delthas[i] = errores[i] * derivada_hiperbolica(suma_ponderada(self.neuronas[i].bias, entrada, self.neuronas[i].pesos))
 
 	def calcular_delthas_ocultas(self, id_funciones, entrada, capa_sig):
-		"""
-		id_funciones        Vector de indices que indican la funcion de 
-		                    activacion asociada a la neurona.
-
-		entrada             Vector de valores presinapticos de entrada de
-		                    la capa.
-
-		capa_sig            Capa neuronal siguiente a la actual.
-
-		Calcula los errores deltha cometidos por cada neurona de las capas
-		ocultas de la red.
-		"""
 		for i in range(len(self.neuronas)):
 			suma_deltha = 0.0
 			for j in range(len(capa_sig.neuronas)):
-				pesos = capa_sig.neuronas[j].pesos
-				suma_deltha += (capa_sig.delthas[j] * pesos[i])
+				suma_deltha += (capa_sig.delthas[j] * capa_sig.neuronas[j].pesos[i])
 			if id_funciones[i] == 1:
 				self.delthas[i] = derivada_lineal(suma_ponderada(self.neuronas[i].bias, entrada, self.neuronas[i].pesos)) * suma_deltha
 			elif id_funciones[i] == 2:
@@ -140,27 +62,12 @@ class CapaNeuronal:
 				self.delthas[i] = derivada_hiperbolica(suma_ponderada(self.neuronas[i].bias, entrada, self.neuronas[i].pesos)) * suma_deltha
 
 	def calcular_salidas(self, id_funciones, entrada):
-		"""
-		id_funciones        Vector de indices que indican la funcion de 
-		                    activacion asociada a la neurona.
-
-		entrada             Vector de valores presinapticos de entrada de
-		                    la capa.
-
-		Calcula las salidas postsinapticas de cada neurona de la capa.
-		"""
 		for i in range(len(id_funciones)):
 			self.neuronas[i].calcular_salida(id_funciones[i], entrada)
 
 
 class RedNeuronal:
-	"""
-	Red neuronal artificial tipo perceptron multicapa.
-	"""
-
 	def __init__(self, total_args, nombre, config_alphas, config_funcns, indices_funcns):
-		"""
-		"""
 		self.nombre_red = nombre
 		self.configuracion_alphas = config_alphas
 		self.configuracion_funciones = config_funcns
@@ -174,26 +81,22 @@ class RedNeuronal:
 					len(self.indice_funcion_activacion[i-1])))
 
 	def establecer_valores_alphas(self, indice):
-		"""
-		"""
 		if indice == 1:
-			alpha = pseudoaleatorio(0.0, 1.0)
+			alpha = pseudoaleatorio(0.0, 0.9)
 			for i in range(len(self.capas)):
 				for j in range(len(self.capas[i].neuronas)):
 					self.capas[i].neuronas[j].alpha = alpha
 		elif indice == 2:
 			for i in range(len(self.capas)):
-				alpha = pseudoaleatorio(0.0, 1.0)
+				alpha = pseudoaleatorio(0.0, 0.9)
 				for j in range(len(self.capas[i].neuronas)):
 					self.capas[i].neuronas[j].alpha = alpha
 		elif indice == 3:
 			for i in range(len(self.capas)):
 				for j in range(len(self.capas[i].neuronas)):
-					self.capas[i].neuronas[j].alpha = pseudoaleatorio(0.0, 1.0)
+					self.capas[i].neuronas[j].alpha = pseudoaleatorio(0.0, 0.9)
 
 	def aplicar_red_neuronal(self, entrada):
-		"""
-		"""
 		salidas = []
 		for i in range(len(entrada)):
 			self.realizar_propagacion(entrada[i])
@@ -205,8 +108,6 @@ class RedNeuronal:
 		return salidas
 
 	def realizar_propagacion(self, entrada):
-		"""
-		"""
 		for i in range(len(self.capas)):
 			if i == 0:
 				self.capas[i].calcular_salidas(self.indice_funcion_activacion[i], entrada)
@@ -218,8 +119,6 @@ class RedNeuronal:
 				self.capas[i].calcular_salidas(self.indice_funcion_activacion[i], entrada)
 
 	def realizar_retropropagacion(self, salidas, entrada):
-		"""
-		"""
 		for i in range(len(self.capas)):
 			indice = len(self.capas) - (i+1)
 			neuronas_actuales = self.capas[indice].neuronas
@@ -250,15 +149,12 @@ class RedNeuronal:
 						capa_siguiente)
 
 	def actualizar_parametros_neuronales(self, entrada):
-		"""
-		"""
 		for i in range(len(self.capas)):
 			self.capas[i].actualizar_biases()
 			if i == 0:
 				self.capas[i].actualizar_pesos(entrada)
 			else:
-				neuronas = self.capas[i-1].neuronas
 				entrada = []
-				for j in range(len(neuronas)):
-					entrada.append(neuronas[j].salida)
+				for j in range(len(self.capas[i-1].neuronas)):
+					entrada.append(self.capas[i-1].neuronas[j].salida)
 				self.capas[i].actualizar_pesos(entrada)
---
+++ proyectoMPI/version2/respaldo_funciones.py
@@ -0,0 +1,109 @@
+"""
+Modulo que contiene las funciones necesarias para calcular los valores
+necesarios para ponderar y ajustar los parametros variables de la red.
+"""
+
+import math
+import random
+
+def pseudoaleatorio(inf, sup):
+	"""
+	inf        Limite inferior del rango.
+
+	sup        Limite superior del rango.
+
+	Regresa un numero real pseudoaleatorio de un rango determinado.
+	"""
+	while True:
+		num_psa = random.uniform(inf, sup)
+		if num_psa != 0.0:
+			return num_psa
+
+def suma_ponderada(bias, entrada, pesos):
+	"""
+	bias        Bias(umbral) sinaptico de la neurona.
+	
+	entrada     Entrada presinaptica de la neurona.
+	
+	pesos       Pesos sinapticos de la neurona.
+
+	(Regla de propagacion neuronal) Regresa el valor del potencial sinaptico
+	de cada neurona de la red.
+	"""
+	potencial = 0.0
+	for i in range(len(pesos)):
+		potencial += (pesos[i] * entrada[i])
+	potencial += bias
+	return potencial
+
+def identidad_lineal(potencial):
+	"""
+	potencial        Potencial sinaptico de la neurona.
+
+	Regresa el valor de activacion postsinaptico de la neurona mediante la
+	funcion de transferencia identidad lineal.
+	"""
+	return potencial
+
+def sigmoide_logistico(potencial):
+	"""
+	potencial        Potencial sinaptico de la neurona.
+
+	Regresa el valor de activacion postsinaptico de la neurona mediante la
+	funcion de transferencia sigmoide logistico.
+	"""
+	return (1.0 / (1.0 + (pow(math.e, (-1.0 * potencial)))))
+
+def sigmoide_tangencial(potencial):
+	"""
+	potencial        Potencial sinaptico de la neurona.
+
+	Regresa el valor de activacion postsinaptico de la neurona mediante la
+	funcion de transferencia sigmoide tangencial.
+	"""
+	return ((2.0 / (1.0 + pow(math.e, (-1.0 * potencial)))) - 1.0)
+
+def sigmoide_hiperbolico(potencial):
+	"""
+	potencial        Potencial sinaptico de la neurona.
+
+	Regresa el valor de activacion postsinaptico de la neurona mediante la
+	funcion de transferencia sigmoide hiperbolico.
+	"""
+	return ((pow(math.e, potencial) - (pow(math.e, (-1.0 * potencial)))) / (pow(math.e, potencial) + (pow(math.e, (-1.0 * potencial)))))
+
+def derivada_lineal(potencial):
+	"""
+	potencial        Potencial sinaptico de la neurona.
+
+	Regresa el valor de la derivada del valor de activacion postsinaptico de
+	la neurona para la funcion de transferencia identidad lineal.
+	"""
+	return 1.0
+
+def derivada_logistica(potencial):
+	"""
+	potencial        Potencial sinaptico de la neurona.
+
+	Regresa el valor de la derivada del valor de activacion postsinaptico de
+	la neurona para la funcion de transferencia sigmoide logistico.
+	"""
+	return (sigmoide_logistico(potencial) * (1.0 - sigmoide_logistico(potencial)))
+
+def derivada_tangencial(potencial):
+	"""
+	potencial        Potencial sinaptico de la neurona.
+
+	Regresa el valor de la derivada del valor de activacion postsinaptico de
+	la neurona para la funcion de transferencia sigmoide tangencial.
+	"""
+	return ((2.0 * pow(math.e, (-1 * potencial))) / pow(1.0 + pow(math.e, (-1 * potencial)), 2.0))
+
+def derivada_hiperbolica(potencial):
+	"""
+	potencial        Potencial sinaptico de la neurona.
+
+	Regresa el valor de la derivada del valor de activacion postsinaptico de
+	la neurona para la funcion de transferencia sigmoide hiperbolico.
+	"""
+	return (1.0 - pow(sigmoide_hiperbolico(potencial), 2.0))
