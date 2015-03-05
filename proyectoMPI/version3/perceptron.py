abcd5a438c9570736bf38ac1f1e351fcee459478
EmanuelGP <gpe_fc@uaem.mx>
2015-03-05 13:29:13 -0600

    Actualizacion.

--- proyectoMPI/version3/perceptron.py
+++ proyectoMPI/version3/perceptron.py
@@ -24,3 +24,30 @@ class Neurona(object):
 		elif id_funcion == 4:
 			self.salida = sigmoide_hiperbolico(suma_ponderada(self.bias,
 											entrada,self.pesos))
+
+class CapaNeuronal(object):
+	"""docstring for CapaNeuronal"""
+	def __init__(self,total_neurs,total_args,indices_funciones):
+		super(CapaNeuronal, self).__init__()
+		self.indices_funciones = indices_funciones
+		self.delthas = [0.0]*total_neurs
+		self.salidas = [0.0]*total_neurs
+		self.entradas = [0.0]*total_args
+		self.neuronas = []
+		for i in range(total_neurs):
+			self.neuronas.append(Neurona(total_args))
+
+	def ajustar_biases(self):
+		pass
+
+	def ajustar_pesos(self):
+		pass
+
+	def calcular_delthas_salida(self):
+		pass
+
+	def calcular_delthas_ocultas(self):
+		pass
+
+	def calcular_salidas(self):
+		pass
