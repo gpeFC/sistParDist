efa25fb6f70cf3f85618d944ad5fe27eb3367a48
EmanuelGP <gpe_fc@uaem.mx>
2015-03-11 16:53:04 -0600

    Actualizacion.

--- proyectoMPI/versionSingular/pmcr.py
+++ proyectoMPI/versionSingular/pmcr.py
@@ -11,6 +11,7 @@ def suma_ponderada(bias, entrada, pesos):
 	potencial = 0.0
 	for i in range(len(pesos)):
 		potencial += (pesos[i] * entrada[i])
+	potencial += bias
 	return potencial
 
 def sigmoide_tangencial(potencial):
@@ -32,77 +33,72 @@ class Neurona(object):
 
 	def calcular_salida(self, id_funcion, entrada):
 		if id_funcion == 1:
-			self.salida = suma_ponderada(self.bias,
-											entrada, self.pesos)
+			self.salida = suma_ponderada(self.bias, entrada, self.pesos)
 		elif id_funcion == 2:
-			self.salida = sigmoide_tangencial(suma_ponderada(self.bias,
-											entrada, self.pesos))
+			self.salida = sigmoide_tangencial(suma_ponderada(self.bias, entrada, self.pesos))
+
+
+class CapaNeuronal(object):
+	"""docstring for CapaNeuronal"""
+	def __init__(self, total_neurs, total_args):
+		super(CapaNeuronal, self).__init__()
+		self.delthas = []
+		self.neuronas = []
+		for i in range(total_neurs):
+			self.neuronas.append(Neurona(total_args))
+
+	def ajustar_biases(self):
+		for i in range(len(self.neuronas)):
+			self.neuronas[i].bias += (self.neuronas[i].alpha * self.delthas[i])
+
+	def ajustar_pesos(self, entrada):
+		for i in range(len(self.neuronas)):
+			for j in range(len(self.neuronas[i].pesos)):
+				self.neuronas[i].pesos[j] -= (self.neuronas[i].alpha * self.delthas[i] * entrada[j])
+
+	def calcular_delthas_salida(self, id_fncns, errores, entrada):
+		for i in range(len(errores)):
+			deltha = 0.0
+			if id_fncns[i] == 1:
+				deltha = errores[i]
+				self.delthas.append(errores[i])
+			else:
+				deltha = errores[i] * derivada_tangencial(suma_ponderada(self.neuronas[i].bias, entrada, self.neuronas[i].pesos))
+			self.delthas.append(deltha)
+
+	def calcular_delthas_ocultas(self, id_fncns, entrada, capa_sig):
+		for i in range(len(id_fncns)):
+			suma_delthas = 0.0
+			for j in range(len(capa_sig.delthas)):
+				suma_delthas += (capa_sig.delthas[j] * capa_sig.neuronas[j].pesos[i])
+			deltha = 0.0
+			if id_fncns[i] == 1:
+				deltha = suma_delthas
+			else:
+				deltha = suma_delthas * derivada_tangencial(suma_ponderada(self.neuronas[i].bias, entrada, self.neuronas[i].pesos))
+			self.delthas.append(deltha)
+
+	def calcular_salidas(self, id_fncns):
+		for i in range(len(id_fncns)):
+			self.neuronas[i].calcular_salida(id_fncns[i], self.entrada)
+
 
 class RedNeuronal(object):
-			"""docstring for RedNeuronal"""
-			def __init__(self, indice_alphas, total_args, indice_fncns):
-				super(RedNeuronal, self).__init__()
-				self.indice_funciones = indice_fncns
-				self.delthas = []
-				self.capas_neurales = []
-				for i in range(len(self.indice_funciones)):
-					capa = []
-					for j in range(len(self.indice_funciones[i])):
-						if i == 0:
-							capa.append(Neurona(total_args))
-						else:
-							capa.append(Neurona(len(self.indice_funciones[i-1])))
-					self.capas_neurales.append(capa)
-				if indice_alphas == 1:
-					alpha = pseudoaleatorio(0.0, 1.0)
-					for i in range(len(self.capas_neurales)):
-						for j in range(len(self.capas_neurales[i])):
-							self.capas_neurales[i][j].alpha = alpha
-				elif indice_alphas == 2:
-					for i in range(len(self.capas_neurales)):
-						alpha = pseudoaleatorio(0.0, 1.0)
-						for j in range(len(self.capas_neurales[i])):
-							self.capas_neurales[i][j].alpha = alpha
-				elif indice_alphas == 3:
-					for i in range(len(self.capas_neurales)):
-						for j in range(len(self.capas_neurales[i])):
-							self.capas_neurales[i][j].alpha = pseudoaleatorio(0.0, 1.0)
+	"""docstring for RedNeuronal"""
+	def __init__(self, id_alphas, total_args, indice_funciones):
+		super(RedNeuronal, self).__init__()
+		self.indice_funciones = indice_funciones
+		self.capas = []
 
-			def aplicar_red(self):
-				pass
+	def aplicar_red(self):
+		pass
 
-			def propagacion(self, entrada):
-				for i in range(len(self.capas_neurales)):
-					for j in range(len(self.capas_neurales[i])):
-						if i == 0:
-							self.capas_neurales[i][j].calcular_salida(self.indice_funciones[i][j], entrada)
-						else:
-							entrada = []
-							for k in range(len(self.capas_neurales[i-1])):
-								entrada.append(self.capas_neurales[i-1][k].salida)
-							self.capas_neurales[i][j].calcular_salida(self.indice_funciones[i][j], entrada)
+	def propagacion(self, entrada):
+		pass
 
-			def retropropagacion(self, salidas, entrada):
-				for i in self.capas_neurales.__reversed__():
-					delthas = []
-					if i == len(self.capas_neurales) - 1:
-						errores = []
-						for j in range(len(self.capas_neurales[i])):
-							errores.append((salidas[j] - self.capas_neurales[i][j].salida))
-						entrada = []
-						for j in range(len(self.capas_neurales[i-1])):
-							entrada.append(self.capas_neurales[i-1][j].salida)
-						for j in range(len(self.capas_neurales[i])):
-							if self.indice_funciones[i][j] == 1:
-								delthas.append(errores[j])
-							else:
-								delthas.append((derivada_tangencial(suma_ponderada(self.capas_neurales[i][j].bias, entrada, self.capas_neurales[i][j].pesos))*errores[j]))
-						self.delthas.append(delthas)
-					else:
-						if i == 0:
-							pass
-						self.delthas.append(delthas)
+	def retropropagacion(self):
+		pass
 
-			def ajustar_parametros(self):
-				pass
-
+	def ajustar_parametros(self):
+		pass
+		
