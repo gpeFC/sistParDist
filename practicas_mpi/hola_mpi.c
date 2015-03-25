043c131d298f158931d435a9049f3719763ff95e
EmanuelGP <emanuelgp@maquina3.(none)>
2015-03-25 14:34:58 -0600

    Practicas MPI.

---
+++ practicas_mpi/hola_mpi.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+int main(int argc, char **argv){
+	int myid, numprocs;
+	FILE *arch;
+	MPI_Init(&argc, &argv);
+		MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+		MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+		printf("Soy el procesador %d de un total de %d\n", myid, numprocs);
+		if(myid == 1){
+			arch = fopen("/home/emanuelgp/practicas_mpi/archivo.txt", "w");
+			fprintf(arch, "Hola soy el procesador 1\n");
+			fclose(arch);
+		}
+	MPI_Finalize();
+	return 0;
+}
