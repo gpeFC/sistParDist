e6c0b873a996e0924eae0394a78d8c50cd7a46e2
EmanuelGP <emanuelgarp@outlook.com>
2015-04-23 21:32:30 -0500

    Actualizacion.

--- sistDistb/tarea/echo_client.py
+++ sistDistb/tarea/echo_client.py
@@ -4,7 +4,7 @@
 
 
 import socket
-m=raw_input()
+m=raw_input("=> ")
 while m:
 	s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 	s.connect(('localhost',5555))
@@ -12,4 +12,4 @@ while m:
 	response=s.recv(1024)
 	s.close()
 	print response
-	m=raw_input()
+	m=raw_input("=> ")
